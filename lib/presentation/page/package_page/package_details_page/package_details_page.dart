03c337d99883fd325cbcb3863618004edaf684ef
Zakiroglu25 <bxtyr1903@gmail.com>
2021-11-02 22:50:00 +0400

    test

---
+++ lib/presentation/page/package_page/package_details_page/package_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PackageDetailsPage extends StatelessWidget {
+  const PackageDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

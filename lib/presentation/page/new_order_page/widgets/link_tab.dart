299acdd501598c9070462ea9a752987907bc4989
Zakiroglu25 <bxtyr1903@gmail.com>
2021-11-24 13:06:01 +0400

    order page ui changed

--- lib/presentation/page/new_order_page/widgets/link_tab.dart
+++ lib/presentation/page/new_order_page/widgets/link_tab.dart
@@ -1,45 +1,26 @@
+import 'package:caspa_v2/app.dart';
 import 'package:caspa_v2/presentation/page/home_page/widgets/section_name.dart';
 import 'package:caspa_v2/util/constants/paddings.dart';
-import 'package:caspa_v2/util/constants/sized_box.dart';
+import 'package:caspa_v2/widget/caspa_appbar/caspa_appbar.dart';
 import 'package:caspa_v2/widget/general/caspa_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class LinkTab extends StatelessWidget {
+class LinkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ListView(
-          padding: Paddings.paddingH16 +
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          children: [
-            MySizedBox.h24,
-            SectionName(
-              title: "İnformasiya",
-            ),
-            MySizedBox.h16,
-            CaspaField(
-              title: "Məhsulun linki",
-            ),
-            CaspaField(
-              title: "Məhsulun sayı",
-              suffixText: "ədəd",
-            ),
-           // GenderFieldRegister(),
-            //GenderFieldRegister(),
-            Wrap(
-              children: [
-                CaspaField(title: "Qiymət",),
-              ],
-            ),
-
-            //burda mehsul novu olacaq
-            //burda ,ehsulun alt novu
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
         ),
-      ],
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Yeni sifariş",
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+      ),
     );
   }
 }

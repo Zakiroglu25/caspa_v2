afa7e05f90c8a82a4d1b4fc019bc065d030decd4
Zakiroglu25 <bxtyr1903@gmail.com>
2021-12-08 18:02:29 +0400

    app info page finish

--- lib/presentation/page/settings_page/settings_page.dart
+++ lib/presentation/page/settings_page/settings_page.dart
@@ -1,8 +1,10 @@
 import 'package:caspa_v2/presentation/page/home_page/widgets/section_name.dart';
+import 'package:caspa_v2/presentation/page/settings_page/app_info_page.dart';
 import 'package:caspa_v2/util/constants/app_text_styles.dart';
 import 'package:caspa_v2/util/constants/assets.dart';
 import 'package:caspa_v2/util/constants/sized_box.dart';
 import 'package:caspa_v2/util/constants/text.dart';
+import 'package:caspa_v2/util/delegate/navigate_utils.dart';
 import 'package:caspa_v2/widget/caspa_appbar/caspa_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -50,16 +52,21 @@ class SettingsPage extends StatelessWidget {
               ],
             ),
             MySizedBox.h45,
-            Row(
-              children: [
-                SvgPicture.asset(Assets.infoApp),
-                MySizedBox.w20,
-                Text(
-                  "Tətbiq haqqında",
-                  style: AppTextStyles.sanF400
-                      .copyWith(color: Colors.black, fontSize: 17.sp),
-                ),
-              ],
+            InkWell(
+              onTap: (){
+                Go.to(context, AppInfoPage());
+              },
+              child: Row(
+                children: [
+                  SvgPicture.asset(Assets.infoApp),
+                  MySizedBox.w20,
+                  Text(
+                    "Tətbiq haqqında",
+                    style: AppTextStyles.sanF400
+                        .copyWith(color: Colors.black, fontSize: 17.sp),
+                  ),
+                ],
+              ),
             ),
           ],
         ),

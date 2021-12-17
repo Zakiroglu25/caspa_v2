4722959f942e5c6a43e639cdcf1ac4a6956383a0
Sohrab Vahidli <63860581+sohrabonline@users.noreply.github.com>
2021-12-17 17:51:42 +0400

    shop lis animation added

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:caspa_v2/infrastructure/cubits/authentication/authentication_cubit.dart';
 import 'package:caspa_v2/infrastructure/cubits/authentication/authentication_state.dart';
-import 'package:caspa_v2/util/delegate/my_printer.dart';
 import 'package:caspa_v2/widget/general/caspa_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,34 +11,32 @@ import 'widget/general/no_data_widget.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return BlocBuilder<AuthenticationCubit, AuthenticationState>(
         builder: (context, state) {
-
-          if (state is AuthenticationSplash) {
-            return SplashPage();
-          } else if (state is AuthenticationLoading) {
-            return Scaffold(
-              backgroundColor: MyColors.backMainColor,
-              body: CaspaLoading(),
-            );
-          } else if (state is AuthenticationServerError) {
-            return SafeArea(
-              child: Scaffold(
-                  body: NoData(
-                    text: "server_error",
-                    refreshButton: () {
-                      context.read<AuthenticationCubit>()
-                        ..startApp(context, showSplash: false);
-                    },
-                  )),
-            );
-          }
-          if (state is AuthenticationUninitialized) {
-            return Pager.login;
-          } else {
-            return Pager.landing;
-          }
-        });
+      if (state is AuthenticationSplash) {
+        return SplashPage();
+      } else if (state is AuthenticationLoading) {
+        return Scaffold(
+          backgroundColor: MyColors.backMainColor,
+          body: CaspaLoading(),
+        );
+      } else if (state is AuthenticationServerError) {
+        return SafeArea(
+          child: Scaffold(
+              body: NoData(
+            text: "server_error",
+            refreshButton: () {
+              context.read<AuthenticationCubit>()
+                ..startApp(context, showSplash: false);
+            },
+          )),
+        );
+      }
+      if (state is AuthenticationUninitialized) {
+        return Pager.login;
+      } else {
+        return Pager.landing;
+      }
+    });
   }
 }

5878e9dd4ecbef262d4272a855aa238499e26cf9
Sohrab Vahidli <63860581+sohrabonline@users.noreply.github.com>
2021-12-26 23:40:21 +0400

    packages fetched

--- lib/presentation/page/package_details_page/package_details_page.dart
+++ lib/presentation/page/package_details_page/package_details_page.dart
@@ -3,6 +3,7 @@ import 'package:caspa_v2/util/constants/assets.dart';
 import 'package:caspa_v2/util/constants/colors.dart';
 import 'package:caspa_v2/util/constants/sized_box.dart';
 import 'package:caspa_v2/util/extensions/smart.dart';
+import 'package:caspa_v2/widget/caspa_appbar/caspa_appbar.dart';
 import 'package:caspa_v2/widget/main/sliver_caspa_bar/sliver_caspa_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -11,8 +12,10 @@ import 'widget/tabs/info_page.dart';
 import 'widget/status_items.dart';
 
 class PackageDetailsPage extends StatelessWidget {
- final Package ? package;
+  final Package? package;
+
   PackageDetailsPage({required this.package});
+
   final tabs = [
     Tab(
       text: 'Status',
@@ -22,6 +25,7 @@ class PackageDetailsPage extends StatelessWidget {
       text: 'Məlumat',
     ),
   ];
+
   //  List<Widget> tabPages = [
   //   StatusTab(),
   //   InfoTab(package: package!,),
@@ -30,17 +34,15 @@ class PackageDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: CaspaAppbar(
+        user: false,
+        title: package!.store,
+        color: MyColors.yellow255,
+        notification: false,
+      ),
       body: SafeArea(
-        child: SliverCaspaBar(
-          tabs: tabs,
-          tabPages: [
-            StatusTab(),
-            InfoTab(package: package!,),
-          ],
-          title: package?.store,
-          back: true,
-          appbarHeight: 1,
-          sliverChild: Container(),
+        child: InfoTab(
+          package: package!,
         ),
       ),
     );

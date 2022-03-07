9b4f1901ec579dfba64840166dffdd9b4e5438f6
Sohrab Vahidli <63860581+sohrabonline@users.noreply.github.com>
2022-03-08 02:41:09 +0400

    status error solved

--- lib/presentation/page/package_details_page/widget/stepper.dart
+++ lib/presentation/page/package_details_page/widget/stepper.dart
@@ -5,6 +5,7 @@ import 'package:caspa_v2/util/constants/app_text_styles.dart';
 import 'package:caspa_v2/util/constants/colors.dart';
 import 'package:caspa_v2/util/constants/margins.dart';
 import 'package:caspa_v2/util/constants/paddings.dart';
+import 'package:caspa_v2/util/constants/physics.dart';
 import 'package:caspa_v2/util/constants/sized_box.dart';
 import 'package:caspa_v2/util/constants/status_list.dart';
 import 'package:caspa_v2/util/delegate/random.dart';
@@ -24,88 +25,95 @@ class PackageStepper extends StatelessWidget {
     return Center(
       child: SizedBox(
         height: titles != null ? 80 : 40,
-        child: Column(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
           children: [
-            Container(
-              height: 40,
-              child: ListView.separated(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  //  final mainAGreement=StatusList.fromStatusToStep(element.status!) == titles[index];
+            Column(
+              children: [
+                Container(
+                  height: 40,
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    physics: Physics.never,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      //  final mainAGreement=StatusList.fromStatusToStep(element.status!) == titles[index];
 
-                  return WidgetOrEmpty(
-                    value: currentStatustes.any((element) =>
-                        StatusList.fromStatusToStep(element.status!) ==
-                        titles[index]),
-                    elseChild: Spin(
-                        child: RotatedBox(
-                            quarterTurns: (currentStatustes.isNotEmpty &&
-                                    StatusList.fromStatusToStep(
-                                            currentStatustes.last.status!) ==
-                                        titles[index])
-                                ? 1
-                                : 0,
-                            child: MinusIcon())),
-                    child: Pulse(child: CheckIcon()),
-                  );
-                },
-                itemCount: titles.length,
-                separatorBuilder: (context, index) {
-                  return Center(
-                    child: WidgetOrEmpty(
-                      value: currentStatustes.any((element) =>
-                          StatusList.fromStatusToStep(element.status!) ==
-                          titles[index]),
-                      elseChild: Container(
-                        margin: Margins.marginH4,
-                        width: 34,
-                        height: 2,
-                        color: MyColors.grey226,
-                      ),
-                      child: Flash(
-                        duration: Duration(milliseconds: 800),
-                        child: Container(
-                          margin: Margins.marginH4,
-                          width: 34,
-                          height: 2,
-                          color: MyColors.green,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 40,
-              padding: Paddings.paddingH16,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                    titles.length,
-                    (index) => Column(
-                          children: [
-                            MySizedBox.h10,
-                            Container(
-                              height: 30,
-                              width: 66,
-                              padding: Paddings.paddingH1,
-                              child: Align(
-                                alignment: Alignment.topCenter,
-                                child: Text(
-                                  titles[index],
-                                  overflow: TextOverflow.clip,
-                                  maxLines: 2,
-                                  textAlign: TextAlign.center,
-                                  style: AppTextStyles.sanF400
-                                      .copyWith(fontSize: 12),
-                                ),
-                              ),
+                      return WidgetOrEmpty(
+                        value: currentStatustes.any((element) =>
+                            StatusList.fromStatusToStep(element.status!) ==
+                            titles[index]),
+                        elseChild: Spin(
+                            child: RotatedBox(
+                                quarterTurns: (currentStatustes.isNotEmpty &&
+                                        StatusList.fromStatusToStep(
+                                                currentStatustes
+                                                    .last.status!) ==
+                                            titles[index])
+                                    ? 1
+                                    : 0,
+                                child: MinusIcon())),
+                        child: Pulse(child: CheckIcon()),
+                      );
+                    },
+                    itemCount: titles.length,
+                    separatorBuilder: (context, index) {
+                      return Center(
+                        child: WidgetOrEmpty(
+                          value: currentStatustes.any((element) =>
+                              StatusList.fromStatusToStep(element.status!) ==
+                              titles[index]),
+                          elseChild: Container(
+                            margin: Margins.marginH4,
+                            width: 34,
+                            height: 2,
+                            color: MyColors.grey226,
+                          ),
+                          child: Flash(
+                            duration: Duration(milliseconds: 800),
+                            child: Container(
+                              margin: Margins.marginH4,
+                              width: 34,
+                              height: 2,
+                              color: MyColors.green,
                             ),
-                          ],
-                        )),
-              ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  padding: Paddings.paddingH16,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                        titles.length,
+                        (index) => Column(
+                              children: [
+                                MySizedBox.h10,
+                                Container(
+                                  height: 30,
+                                  width: 66,
+                                  padding: Paddings.paddingH1,
+                                  child: Align(
+                                    alignment: Alignment.topCenter,
+                                    child: Text(
+                                      titles[index],
+                                      overflow: TextOverflow.clip,
+                                      maxLines: 2,
+                                      textAlign: TextAlign.center,
+                                      style: AppTextStyles.sanF400
+                                          .copyWith(fontSize: 12),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )),
+                  ),
+                )
+              ],
             )
           ],
         ),

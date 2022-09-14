321b9815029782ed8cec98a4e7b6474173f8def6
Sohrab Vahidli <63860581+sohrabonline@users.noreply.github.com>
2022-09-15 02:07:16 +0400

    family member added link order problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
 
-import 'package:caspa_v2/mate_app.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'infrastructure/configs/init.dart';
+import 'mate_app.dart';
 
 void main() async {
+  // for (int i = 0; i < 10; i++) {
+  //   if (i.isEven) {
+  //     Future.microtask(() => print("$i"));
+  //   } else {
+  //     Future.delayed(Duration(milliseconds: 1)).whenComplete(() => print("$i"));
+  //   }
+  // }
+
   runZonedGuarded(() async {
     await init();
     runApp(const MateApp());

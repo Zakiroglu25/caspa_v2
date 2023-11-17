907ed3e4af608aeafd08c6ef3e5002d1e5549995
sohrabvahidli <esev.sv@gmail.com>
2023-11-17 12:44:49 +0400

    ol

---
+++ lib/res/i_font_res.dart
@@ -0,0 +1,8 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class FontRes {
+  static const String SFNSTEXT_REGULAR = 'SFNSText-Regular';
+  static const String COHEADLINE = 'coheadline';
+}
